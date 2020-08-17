ab12c3036bc88f3dfa52ec89169f906b6739f4e2
santhsohkumard11 <santhoshkdhana@gmail.com>
2020-08-17 15:25:08 +0530

    commit1

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="quickdataanalysis",
+    version="0.0.1",
+    description=('Kick start your data analysis with these functions'),
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    maintainer="Santhoshkumard11",
+    author='Santhoshkumard11',
+    url='https://github.com/Santhoshkumard11/quickdataanalysis',
+    install_requires=[
+        ' pandas>=1.0.1',
+    ],
+    classifiers=[
+        'Development Status :: 1 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries'
+    ],
+    project_urls=[
+        "Source Code :: https://github.com/Santhoshkumard11/quickdataanalysis"
+    ]
+)
